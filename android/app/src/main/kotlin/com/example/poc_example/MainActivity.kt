d3f428dab2e4a15e1c1c42dc509221390c2f50a0
toshiossada <toshiossada@gmail.com>
2024-08-24 15:10:56 -0300

    init

---
+++ android/app/src/main/kotlin/com/example/poc_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.poc_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: const Center(
+          child: Text('Hello World!'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.start),
+        ),
+      ),
+    );
+  }
+}
